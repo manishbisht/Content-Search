a2eb5ac424ebb84ecece83e4b86e767150972a7c
Manish Bisht <manish.bisht490@gmail.com>
2017-04-10 22:12:57 +0530

    wiki sample data

---
+++ src/wiki.py
@@ -0,0 +1,9 @@
+import urllib, json, re
+query = 'water'
+url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&converttitles=zh&format=json&exintro=&titles=" + query
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+data = data['query']['pages']
+page = data.keys()[0]
+if page != '-1':
+	print re.sub('<[^<]+?>', '', data[page]['extract'].split('\n')[0])
